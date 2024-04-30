716bff5efdc01774e08161a10ecc1e663eba6948
Uanderson <uanderson.lm@gmail.com>
2024-04-29 22:01:30 -0300

    refatoração de rotas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
-
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -14,34 +13,29 @@ conexao.once("open", () => {
 })
 
 const app = express();
+routes(app);
 app.use(express.json());
 
 
-// const buscaLivro = (id) => {
-//     return livros.findIndex(livro => {
-//         return livro.id === Number(id);
-//     })
-// }
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.JS")
-});
+// app.get("/", (req, res) => {
+//     res.status(200).send("Curso de Node.JS")
+// });
 
 
-app.get("/livros", async (req, res) => {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros)
-});
+// app.get("/livros", async (req, res) => {
+//     const listaLivros = await livro.find({});
+//     res.status(200).json(listaLivros)
+// });
 
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     res.status(200).json(livros[index])
 })
 
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("livro cadastrado com sucesso")
-});
+// app.post("/livros", (req, res) => {
+//     livros.push(req.body);
+//     res.status(201).send("livro cadastrado com sucesso")
+// });
 
 app.put("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
